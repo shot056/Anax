6819ea3e1f5f3c52e7abe91915561e2e04608ab0
shot056 <shot056@gmail.com>
2012-11-12 01:45:01 +0900

    change applicants order to date_created

--- lib/Anax/Admin/Applicants.pm
+++ lib/Anax/Admin/Applicants.pm
@@ -19,7 +19,7 @@ sub index {
         or die DBIx::Simple->error;
     $dbis->abstract = SQL::Maker->new( driver => $dbis->dbh->{Driver}->{Name} );
     $dbis->begin_work or die $dbis->error;
-    my $rslt = $dbis->query( "SELECT a.id, a.email, a.date_created, a.date_updated, f.id AS form_id, f.name AS form_name, af.date_created AS af_date_created, af.date_updated AS af_date_updated  FROM applicants AS a, forms AS f, applicant_form AS af WHERE a.is_deleted = FALSE AND a.id=af.applicants_id AND f.id=af.forms_id;" )
+    my $rslt = $dbis->query( "SELECT a.id, a.email, a.date_created, a.date_updated, f.id AS form_id, f.name AS form_name, af.date_created AS af_date_created, af.date_updated AS af_date_updated  FROM applicants AS a, forms AS f, applicant_form AS af WHERE a.is_deleted = FALSE AND a.id=af.applicants_id AND f.id=af.forms_id ORDER BY date_created DESC;" )
         or die $dbis->error;
     $self->render( template => 'admin/applicants/index', datas => $rslt );
     $dbis->commit;
