f7598d2cf9fd2347e26e66393b94efc656779d98
shot056 <shot056@gmail.com>
2014-12-10 19:09:51 +0900

    fix log level

--- lib/Anax.pm
+++ lib/Anax.pm
@@ -24,7 +24,6 @@ sub mkrandstr {
 sub startup {
     my $self = shift;
 
-    $self->app->log->level('debug');
     my $fields = { hash => {},
                    array => [
                              { value => 'textfield', label => 'テキストフィールド' },
